349a545fb486ec1a7e392231767989467ceccea4
james <james@jamesbuntu.(none)>
2011-03-24 16:41:44 +0000

    Added xmonad script for ubuntu

---
+++ xmonad/ubuntu-script.sh
@@ -0,0 +1,7 @@
+xrandr --output DVI-0 --mode 1680x1050
+xrandr --output DVI-1 --mode 1440x900
+xrandr --output DVI-1 --right-of DVI-0
+
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x191970 --height 12 &
+xscreensaver -nosplash &
+
